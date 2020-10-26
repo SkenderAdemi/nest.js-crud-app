becea2d5a4dfbdf1fdb1a3aec48ba0de8e6e064f
Skender Ademi <skender@giggle.tips>
2020-10-26 16:56:03 +0100

    Initial commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,48 @@
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common'
+
+// service
+import { ProductsService } from './products.service'
+
+@Controller('products')
+export class ProductController {
+
+    constructor(private productsService: ProductsService){}
+
+    @Post()
+    addProduct( 
+        @Body('title') prodTitle:string,
+        @Body('description') prodDesc:string,
+        @Body('price') prodPrice:number
+    ){
+       const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
+
+       return { id: generatedId }
+    }
+
+    @Get()
+    getAllProducts() {
+        return this.productsService.getProducts();
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') prodId: string){
+        return this.productsService.getSingleProduct(prodId)
+    }
+
+    @Patch(':id')
+    updateProduct(
+        @Param('id') prodId: string,
+        @Body('title') prodTitle:string,
+        @Body('description') prodDesc:string,
+        @Body('price') prodPrice:number
+    ){
+         this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+         return null;
+    }
+
+    @Delete(':id')
+    removeProduct(@Param('id') prodId: string){
+        this.productsService.removeProduct(prodId);
+        return null
+    }
+}
